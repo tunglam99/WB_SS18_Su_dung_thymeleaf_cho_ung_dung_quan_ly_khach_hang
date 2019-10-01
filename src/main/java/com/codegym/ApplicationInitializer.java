c20b02d2ea7f9c784ce5011d78dd242c353728ef
Nguyen Tung Lam <lamnthnue.edu.vn@gmail.com>
2019-10-01 16:07:41 +0700

    'ok'

--- src/main/java/com/codegym/ApplicationInitializer.java
+++ src/main/java/com/codegym/ApplicationInitializer.java
@@ -1,6 +1,9 @@
 package com.codegym;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,6 +19,13 @@ public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherSe
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+    @Override
+    protected Filter[] getServletFilters() {
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return new Filter[] { characterEncodingFilter};
     }
 
 
